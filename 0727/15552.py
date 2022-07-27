b2b4d4078fa5be28e6962f39260bb3772298521c
SJPark97 <tjwlsdud33@naver.com>
2022-07-27 16:41:53 +0900

    finish 0727

--- 0727/15552.py
+++ 0727/15552.py
@@ -2,3 +2,10 @@ T = int(input())
 for number in range(T):
     A, B = map(int, input().split())
     print(A + B)
+
+import sys
+
+T = int(sys.stdin.readline())
+for number in range(T):
+    A, B = map(int, sys.stdin.readline().split())
+    print(A + B)
