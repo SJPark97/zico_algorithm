64885a1812ec28004ddf80df74f3a6ebf2437f8b
SJPark97 <tjwlsdud33@naver.com>
2022-07-31 19:33:14 +0900

    add 2738

--- 0730/2738.py
+++ 0730/2738.py
@@ -1,1 +1,11 @@
 N, M = map(int, input().split())
+A = []
+B = []
+for n in range(N):
+    A.append(list(map(int, input().split())))
+for n in range(N):
+    B.append(list(map(int, input().split())))
+for n in range(N):
+    for m in range(M):
+        A[n][m] += B[n][m]
+    print(*A[n])
