99098b9b56c67a4252cc621fe07208e0c565ea8d
SJPark97 <tjwlsdud33@naver.com>
2022-08-05 22:06:56 +0900

    add 2068answer

--- 0804/SWEA_2068.py
+++ 0804/SWEA_2068.py
@@ -1,8 +1,14 @@
+# T = int(input())
+# for case in range(T):
+#     number_list = list(map(int, input().split()))
+#     highest_number = 0
+#     for number in number_list:
+#         if highest_number < number:
+#             highest_number = number
+#     print(f'#{case + 1} {highest_number}')
+
 T = int(input())
 for case in range(T):
     number_list = list(map(int, input().split()))
-    highest_number = 0
-    for number in number_list:
-        if highest_number < number:
-            highest_number = number
+    highest_number = max(number_list)
     print(f'#{case + 1} {highest_number}')
