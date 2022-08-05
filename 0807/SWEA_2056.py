1ffe3cbde33a20b7aaf2b3822e7a26c958e2175f
SJPark97 <tjwlsdud33@naver.com>
2022-08-05 17:26:03 +0900

    fix 0807

--- 0807/SWEA_2056.py
+++ 0807/SWEA_2056.py
@@ -4,14 +4,12 @@ for t in range(1, 1 + T):
     year = int(input_day[:4])
     month = int(input_day[4:6])
     day = int(input_day[6:])
-    if month < 1 or month > 12:
-        print(f'#{t} -1')
-    elif day < 1 or day > 31:
+    if month == 0 or month > 12 or day == 0 or day > 31 or year < 1:
         print(f'#{t} -1')
     elif month == 2 and day > 28:
         print(f'#{t} -1')
     elif month == 4 or month == 6 or month == 9 or month == 11:
-        if day > 30:
+        if day == 31:
             print(f'#{t} -1')
         else:
             print(f'#{t} {input_day[:4]}/{input_day[4:6]}/{input_day[6:]}')
