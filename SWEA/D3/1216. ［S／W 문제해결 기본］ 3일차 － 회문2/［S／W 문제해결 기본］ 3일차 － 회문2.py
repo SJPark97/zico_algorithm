c02652ed839b45f7fae6b1f016ac552f8812bd6d
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-08-16 11:54:37 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 569 ms, Memory: 67,244 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,14 +1,19 @@
 def circular(string_list):
-    string_list_row = string_list
-    string_list_col = list(zip(*string_list))
-    for M in range(100, 0, -1):
+    for M in range(100, 2, -1):
         for i in range(100):
             for j in range(100-M+1):
-                word_row = string_list_row[i][j:M+j]
-                word_col = string_list_col[i][j:M+j]
-                if word_row == word_row[::-1]:
-                    return M
-                elif word_col == word_col[::-1]:
+                find = True
+                find_row = True
+                find_col = True
+                for checking in range(M//2):
+                    if find_row and string_list[i][j+checking] != string_list[i][j+M-1-checking]:
+                        find_row = False
+                    if find_col and string_list[j+checking][i] != string_list[j+M-1-checking][i]:
+                        find_col = False
+                    if not find_row and not find_col:
+                        find = False
+                        break
+                if find:
                     return M
 
 for test_case in range(10):
