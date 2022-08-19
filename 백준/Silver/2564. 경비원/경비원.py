f480d7efcf3bb90fd21337e9a72193c0a0f55ae2
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-08-19 22:49:40 +0900

    [Silver I] Title: 경비원, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.py
+++ 백준/Silver/2564. 경비원/경비원.py
@@ -18,12 +18,14 @@ for n in range(store_num + 1):
 count = 0
 
 for v in range(store_num):
+    # if stores[-1][0] + stores[v][0] == 3:
     if stores[-1][0] == 1 and stores[v][0] == 2 or stores[-1][0] == 2 and stores[v][0] == 1:
         w_len = stores[v][2] + stores[-1][2]
         if w_len > w:
             count += w * 2 - w_len + h
         else:
             count += w_len + h
+    # elif stores[-1][0] + stores[v][0] == 7:
     elif stores[-1][0] == 3 and stores[v][0] == 4 or stores[-1][0] == 4 and stores[v][0] == 3:
         h_len = stores[v][1] + stores[-1][1]
         if h_len > h:
