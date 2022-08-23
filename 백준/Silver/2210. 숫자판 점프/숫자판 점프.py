2cb1d1a3f5cf3f0b07634b8374a9c135281a030a
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-08-23 15:51:16 +0900

    [Silver II] Title: 숫자판 점프, Time: 3016 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -0,0 +1,25 @@
+def board_check(arr, x, y):
+    global result
+    if len(arr) == 6:
+        if arr not in result:
+            result.append(arr.copy())
+        return
+    for n in range(4):
+        nx, ny = x + dx[n], y + dy[n]
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            arr.append(board[x][y])
+            board_check(arr, nx, ny)
+            arr.pop()
+
+
+board = [list(map(int, input().split())) for _ in range(5)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+result = []
+
+for i in range(5):
+    for j in range(5):
+        board_check([], i, j)
+
+print(len(result))
+
