a17ea106b66270c314657352a73c996adf38cbe1
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-08-23 10:06:02 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 150 ms, Memory: 58,544 KB -BaekjoonHub

---
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -0,0 +1,53 @@
+
+
+for test_case in range(1, 11):
+    len_word = input()
+    word = input()
+    result = ''
+    stack = []
+
+    for token in word:
+        if token in '+-/*()':
+            if not stack or token == '(':
+                stack.append(token)
+
+            elif token in '*/':
+                while stack and stack[-1] in '*/':
+                    result += stack.pop()
+                stack.append(token)
+
+            elif token in '+-':
+                while stack and stack[-1] != '(':
+                    result += stack.pop()
+                stack.append(token)
+
+            elif token == ')':
+                while stack and stack[-1] != '(':
+                    result += stack.pop()
+                stack.pop()
+        else:
+            result += token
+    while stack:
+        result += stack.pop()
+
+    for str in result:
+        if str not in '+-*/':
+            stack.append(int(str))
+
+        else:
+            x = stack.pop()
+            y = stack.pop()
+
+            if str == '+':
+                stack.append(y + x)
+            elif str == '-':
+                stack.append(y - x)
+            elif str == '*':
+                stack.append(y * x)
+            elif str == '/':
+                stack.append(y / x)
+
+    print(f'#{test_case} {stack[-1]}')
+
+
+
