48235534795af73515001a19a7efc1a8b30bb3d7
SJPark97 <tjwlsdud33@naver.com>
2022-08-24 17:36:14 +0900

    add 2001, 2005

---
+++ 0823/SWEA_2005.py
@@ -0,0 +1,24 @@
+def pascal(n):
+    if n == 1:
+        pascal_list[0].append(1)
+    else:
+        pascal(n - 1)
+        for i in range(n):
+            try:
+                pascal_list[n-1].append(pascal_list[n-2][i-1]+pascal_list[n-2][i])
+            except:
+                pascal_list[n-1].append(1)
+
+
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    print(f'#{test_case}')
+
+    N = int(input())
+    pascal_list = [[0] for _ in range(N)]
+    pascal(N)
+    for i in pascal_list:
+        print(*i[1:])
