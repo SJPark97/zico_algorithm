6ec623fd1e479bfb0e8b13c8bca9fffff4bc1e12
SJPark97 <tjwlsdud33@naver.com>
2022-08-24 18:19:16 +0900

    add pg12922

--- 0819/BJ_2564.py
+++ 0819/BJ_2564.py
@@ -18,25 +18,25 @@ for n in range(store_num + 1):
 count = 0
 
 for v in range(store_num):
-    # if stores[-1][0] + stores[v][0] == 3:
-    if (
-        stores[-1][0] == 1
-        and stores[v][0] == 2
-        or stores[-1][0] == 2
-        and stores[v][0] == 1
-    ):
+    if stores[-1][0] + stores[v][0] == 3:
+    # if (
+    #     stores[-1][0] == 1
+    #     and stores[v][0] == 2
+    #     or stores[-1][0] == 2
+    #     and stores[v][0] == 1
+    # ):
         w_len = stores[v][2] + stores[-1][2]
         if w_len > w:
             count += w * 2 - w_len + h
         else:
             count += w_len + h
-    # elif stores[-1][0] + stores[v][0] == 7:
-    elif (
-        stores[-1][0] == 3
-        and stores[v][0] == 4
-        or stores[-1][0] == 4
-        and stores[v][0] == 3
-    ):
+    elif stores[-1][0] + stores[v][0] == 7:
+    # elif (
+    #     stores[-1][0] == 3
+    #     and stores[v][0] == 4
+    #     or stores[-1][0] == 4
+    #     and stores[v][0] == 3
+    # ):
         h_len = stores[v][1] + stores[-1][1]
         if h_len > h:
             count += h * 2 - h_len + w
