64efe5e0c30eb8e6de2bcf0250f49130936bd6c5
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-08-26 21:56:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,44 +1,22 @@
 def solution(dartResult):
-    string = dartResult
-    word = ''
-    signal = True
-    for i in range(len(string)):
-        if signal:
-            if string[i].isdigit():
-                word += ' ' + string[i]
-                if string[i+1].isdigit():
-                    word += string[i+1]
-                    string.replace(string[i+1], ' ')
-                    signal = False
-            else:
-                word += string[i]
-        else:
-            signal = True
+    answer = []
+    dartResult = dartResult.replace('10','k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+    bonus = (0, 'S', 'D', 'T')
+    sig = -1
     
-    word_list = list(word.split())
-    result = []
-    signal = []
-    for n in range(3):
-        num = ''
-        for i in word_list[n]:
-            if i.isdigit():
-                num += i
-            elif i == 'S':
-                nu = int(num)
-            elif i == 'D':
-                nu = int(num) ** 2
-            elif i == 'T':
-                nu = int(num) ** 3
-            elif i == '#':
-                nu = nu * (-1)
-            elif i == '*':
-                signal.append(n)
-        result.append(nu)
-    if signal:
-        for i in signal:
-            if i == 0:
-                result[i] = result[i] * 2
+    for i in point:
+        if i.isdigit():
+            answer.append(int(i))
+            sig += 1
+        elif i in bonus:
+            answer[sig] = answer[sig] ** bonus.index(i)
+        elif i == '#':
+            answer[sig] = answer[sig] * (-1)
+        else:
+            if sig == 0:
+                answer[sig] = answer[sig] * 2
             else:
-                result[i-1] = result[i-1] * 2
-                result[i] = result[i] * 2
-    return sum(result)
+                answer[sig-1] = answer[sig-1] * 2
+                answer[sig] = answer[sig] * 2
+    return sum(answer)
