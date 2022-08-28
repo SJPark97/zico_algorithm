c19b02fd76fe82e9e1f4e119ee7dcd2bce4bafc9
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-08-28 18:54:47 +0900

    [D3] Title: 오목 판정, Time: 259 ms, Memory: 64,420 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -3,13 +3,15 @@ from collections import deque
 
 def init():
     n = int(input())
-    board = [list(input()) for _ in range(n)]
+    board = [input() for _ in range(n)]
     return n, board
 
 
 def sol():
     n, board = init()
     for i in range(n):
+        if 'ooooo' in board[i]:
+            return print('YES')
         for j in range(n):
             if board[i][j] == 'o' and chk(board, n, i, j):
                 return print('YES')
@@ -30,8 +32,8 @@ def chk(board, N, i, j):
 
 
 T = int(input())
-dx = [-1, 1, 0, 0, 1, 1, -1, -1]
-dy = [0, 0, -1, 1, 1, -1, 1, -1]
+dx = [-1, 1, 1, 1, -1, -1]
+dy = [0, 0, 1, -1, 1, -1]
 
 for test_case in range(1, T+1):
     print(f'#{test_case}', end=' ')
