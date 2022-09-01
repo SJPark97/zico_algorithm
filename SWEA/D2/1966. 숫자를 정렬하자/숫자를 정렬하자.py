bc1d00ee07a303a4c465e3cee71c0e5086d180de
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-09-01 15:27:18 +0900

    [D2] Title: 숫자를 정렬하자, Time: 146 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -1,11 +1,11 @@
-T = int(input())
-
-for test_case in range(1, T+1):
-    print(f'#{test_case}', end=' ')
-
-    # N 받아오기
+def sol():
     N = int(input())
 
     num_list = sorted(list(map(int, input().split())))
 
     print(*num_list)
+
+
+for test_case in range(1, int(input())+1):
+    print(f'#{test_case}', end=' ')
+    sol()
