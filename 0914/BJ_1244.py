4b86d8d52d96b7eb2034f1408e9a9a89547c48ab
SJPark97 <tjwlsdud33@naver.com>
2022-09-14 20:05:49 +0900

    a

---
+++ 0914/BJ_1244.py
@@ -0,0 +1,21 @@
+n = int(input())
+switch = list(map(int, input().split()))
+for _ in range(int(input())):
+    gender, num = map(int, input().split())
+    if gender == 1:
+        for i in range(num-1, n, num):
+            switch[i] = abs(switch[i]-1)
+    else:
+        num = num - 1
+        switch[num] = abs(switch[num]-1)
+        for j in range(1, n//2):
+            if num - j < 0 or num + j >= n:
+                break
+            elif switch[num-j] == switch[num+j]:
+                switch[num-j] = abs(switch[num-j]-1)
+                switch[num+j] = abs(switch[num+j] - 1)
+            else:
+                break
+for l in range(0, len(switch), 20):
+    p_switch = switch[l:l+20]
+    print(*p_switch)
