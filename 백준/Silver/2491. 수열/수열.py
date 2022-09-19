d525682ddc350e2774d5052bbc4bd71b1cb8807f
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-09-19 14:47:30 +0900

    [Silver IV] Title: 수열, Time: 140 ms, Memory: 38216 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.py
+++ 백준/Silver/2491. 수열/수열.py
@@ -5,23 +5,20 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 up = 1
-up_list = [1]
+up_down_list = []
 down = 1
-down_list = [1]
 for i in range(n - 1):
     if nums[i] < nums[i + 1]:
         up += 1
-        down_list.append(down)
+        up_down_list.append(down)
         down = 1
     elif nums[i] > nums[i + 1]:
         down += 1
-        up_list.append(up)
+        up_down_list.append(up)
         up = 1
     else:
         up += 1
         down += 1
-        up_list.append(up)
-        down_list.append(down)
-up_list.append(up)
-down_list.append(down)
-print(max(max(up_list), max(down_list)))
+        up_down_list.extend([up, down])
+up_down_list.extend([up, down])
+print(max(up_down_list))
