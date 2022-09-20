66915c62db27b7212e653cd15a94fbbdbfc57350
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-09-21 05:05:34 +0900

    [Silver II] Title: 직사각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.py
+++ 백준/Silver/2527. 직사각형/직사각형.py
@@ -1,14 +1,12 @@
 for _ in range(4):
     x1, y1, x2, y2, x3, y3, x4, y4 = map(int, input().split())
-
-    xd = min(x2, x4) - max(x1, x3)
-    yd = min(y2, y4) - max(y1, y3)
-
-    if (xd > 0 and yd > 0):
-        print('a')
-    elif (xd < 0 or yd < 0):
-        print('d')
-    elif (xd == 0 and yd == 0):
+    dx1, dx2 = min(x2, x4), max(x1, x3)
+    dy1, dy2 = min(y2, y4), max(y1, y3)
+    if dx1 == dx2 and dy1 == dy2:
         print('c')
+    elif dx1 < dx2 or dy1 < dy2:
+        print('d')
+    elif dx1 > dx2 and dy1 > dy2:
+        print('a')
     else:
         print('b')
