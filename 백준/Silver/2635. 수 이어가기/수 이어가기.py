2b321df1b308929e851ffb47b92c256c2624c70e
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-09-24 23:32:36 +0900

    [Silver V] Title: 수 이어가기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -7,13 +7,9 @@ def chk(nums):
 
 n = int(input())
 answer = []
-if n == 1:
-    print(4)
-    print(*[1, 1, 0, 1])
-else:
-    for num in range(int(n * 0.61), int(n * 0.7) + 1):
-        chk_nums = chk([n, num])
-        if len(answer) < len(chk_nums):
-            answer = chk_nums[:]
-    print(len(answer))
-    print(*answer)
+for num in range(int(n * 0.61), int(n * 0.62) + 2):
+    chk_nums = chk([n, num])
+    if len(answer) < len(chk_nums):
+        answer = chk_nums[:]
+print(len(answer))
+print(*answer)
