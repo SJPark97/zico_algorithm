318e9a1861edc706835afa7844aa32593516aa98
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-09-24 20:38:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 292 ms, Memory: 63,312 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -22,7 +22,7 @@ def chk(x, y, d):
             if list_append(x, y, up_down, left_right, 0, dessert_list):
                 if list_append(x, y, left_right, up_down, 1, dessert_list):
                     if len(set(dessert_list)) == d * 2:
-                        return d * 2
+                        return d
         d -= 1
     return -1
 
@@ -33,10 +33,12 @@ for test_case in range(1, int(input()) + 1):
     print(f'#{test_case}', end=' ')
     n = int(input())
     board = [list(map(int, input().split())) for _ in range(n)]
-    answer = -1
+    answer = -0.5
     for i in range(n - 2):
         for j in range(1, n - 1):
             result = chk(i, j, n - i - 1)
             if answer < result:
                 answer = result
-    print(answer)
+        if n - 1 - i <= answer:
+            break
+    print(int(answer * 2))
