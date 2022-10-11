1822c7c06369a5de7124b0b8f6590669001b9949
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-10-11 12:45:10 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 365 ms, Memory: 63,012 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,36 +1,27 @@
-def init():
-    N = int(input())
-    num_list = sorted(list(map(int, input().split())))
-    return N, num_list
-
-
-def chk(s):
-    num = s % 10
-    s //= 10
-    while s:
-        n = s % 10
-        if num < n:
-            return False
-        num = n
-        s //= 10
-    return True
-
-
 def sol():
-    answer = -1
-    n, num_list1 = init()
-    for num in range(n-1, -1, -1):
-        for num1 in range(num-1, -1, -1):
-            number = num_list1[num] * num_list1[num1]
-            if answer > number:
-                break
-            if chk(number):
-                answer = number
-    return print(answer)
+    def chk(s):
+        num = s % 10
+        s //= 10
+        while s:
+            n = s % 10
+            if num < n:
+                return False
+            num = n
+            s //= 10
+        return True
+
+    for tc in range(1, int(input()) + 1):
+        answer = -1
+        n = int(input())
+        num_list = sorted(list(map(int, input().split())))
+        for num in range(n - 1, -1, -1):
+            for num1 in range(num - 1, -1, -1):
+                number = num_list[num] * num_list[num1]
+                if answer > number:
+                    break
+                if chk(number):
+                    answer = number
+        print('#{} {}'.format(tc, answer))
 
 
-T = int(input())
-
-for test_case in range(1, T+1):
-    print(f'#{test_case}', end=' ')
-    sol()
+sol()
