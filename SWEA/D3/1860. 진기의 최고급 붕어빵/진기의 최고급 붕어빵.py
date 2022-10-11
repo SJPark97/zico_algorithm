1e5a9a090b181909a191e25be9a09573175c9e5c
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-10-11 15:16:58 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 349 ms, Memory: 63,288 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -2,10 +2,12 @@ def sol():
     for tc in range(1, int(input()) + 1):
         n, m, k = map(int, input().split())
         arrive = sorted(list(map(int, input().split())))
-        make = [i * m for i in range(1, n+1) for _ in range(k)]
+        make = 0
         result = 'Possible'
         for i in range(n):
-            if arrive[i] < make[i]:
+            if i % k == 0:
+                make += m
+            if arrive[i] < make:
                 result = 'Impossible'
                 break
         print(f'#{tc} {result}')
