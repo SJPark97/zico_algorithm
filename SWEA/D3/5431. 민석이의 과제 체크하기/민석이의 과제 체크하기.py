b90540efb4303bf743124dc83c9bb55268ab2f3c
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-10-11 12:41:29 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 415 ms, Memory: 64,116 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,11 +1,9 @@
-T = int(input())
+def sol():
+    for tc in range(1, int(input()) + 1):
+        n, k = map(int, input().split())
+        k_list = sorted(list(set(list(range(1, n + 1))) - set(map(int, input().split()))))
+        print(f'#{tc}', end=" ")
+        print(*k_list)
 
-for test_case in range(1, T+1):
-    print(f'#{test_case}', end=' ')
 
-    n, k = map(int, input().split())
-    k_list = list(map(int, input().split()))
-    for n_list in range(1, n + 1):
-        if n_list not in k_list:
-            print(n_list, end=' ')
-    print()
+sol()
