1290acf9473707fb352ec32aa79d3b29dbaa5f21
Seung Jae Park <109258034+SJPark97@users.noreply.github.com>
2022-10-17 16:53:32 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,12 @@
+def solution(lottos, win_nums):
+    cnt = 0
+    cnt_num = 0
+    for num in lottos:
+        if num == 0:
+            cnt_num += 1
+        if num in win_nums:
+            cnt += 1
+    cnt_num += cnt
+    answer = [7-cnt_num if cnt_num > 1 else 6, 7-cnt if cnt > 1 else 6]
+    print(answer)
+    return answer
